578d7dd36aa49a76de74f41e49be3d702a589023
seungwon0113 <jungseungwon0113@gmail.com>
2025-03-05 13:06:20 +0900

    git rebase

---
+++ math_module/main.py
@@ -0,0 +1,10 @@
+from math_operatios import add, multiply
+
+num1 = 5
+num2 = 3
+
+result_add = add(num1, num2)
+result_multiply = multiply(num1, num2)
+
+print(f"{num1} + {num2} = {result_add}")
+print(f"{num1} * {num2} = {result_multiply}")
