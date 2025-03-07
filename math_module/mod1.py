13679287ce03dd43bee8e24917d597caffd40a55
seungwon0113 <jungseungwon0113@gmail.com>
2025-03-07 12:44:54 +0900

    Revert "git rebase"
    This reverts commit 43f877a66c97d25f17fa149901dd35624971befc.

---
+++ math_module/mod1.py
@@ -0,0 +1,15 @@
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def div(a, b):
+    return a // b
+
+# print(add(1, 5))
+# print(sub(5, 2))
+
+if __name__ == "__main__":
+    print(add(1, 5))
+    print(sub(5, 2))
